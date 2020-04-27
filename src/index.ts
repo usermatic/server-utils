4b46f6f3d2e72e45fe50248aeca3c86085bfc752
Mark Logan <mark@gmail.com>
2020-04-27 10:58:33 -0700

    Add type-aware verification, add verifyReauthToken

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 
+import { Record, String, Array, Static } from 'runtypes'
 import jwt from 'jsonwebtoken'
+
+const uuidRe = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
+
+const isUUID = (s: string) => {
+  return uuidRe.test(s)
+}
+
+const UUID = String.withConstraint((s) => isUUID(s))
+
+export const AuthToken = Record({
+  id: UUID
+})
+
+export type AuthToken = Static<typeof AuthToken>
+
+const ReauthToken = Record({
+  userContents: String,
+  reauthenticationMethods: Array(String)
+})
+
+export type ReauthToken = Static<typeof ReauthToken>
 
 export const verifyJwt = (token: string, key: string): string | object => {
   if (typeof(key) !== 'string' || key.length < 27) {
@@ -10,4 +32,38 @@ export const verifyJwt = (token: string, key: string): string | object => {
   }
 
   return jwt.verify(token, key, { algorithms: ['HS256'] })
+}
+
+export const verifyAuthToken = (token: string, key: string): AuthToken => {
+  const verified = verifyJwt(token, key)
+  if (!AuthToken.guard(verified)) {
+    throw new Error(`malformed AuthToken: ${ JSON.stringify(verified) }`)
+  }
+  return verified
+}
+
+type ReauthenticationMethod = 'password' | 'mfa'
+
+export const verifyReauthToken =
+(token: string, key: string, requiredMethods: ReauthenticationMethod[]): ReauthToken => {
+
+  const contents = verifyJwt(token, key)
+  if (!ReauthToken.guard(contents)) {
+    console.error("malformed reauth token", JSON.stringify(contents))
+    throw new Error("malformed reauth token")
+  }
+  if (requiredMethods.length == 0) {
+    throw new Error("Must supply at least one required re-authentication method")
+  }
+  const missingMethods = requiredMethods.filter(
+    m => !contents.reauthenticationMethods.includes(m)
+  )
+  if (missingMethods.length > 0) {
+    throw new Error(
+      'The following required re-authentication methods were missing: '
+      + missingMethods.join(', ')
+    )
+  }
+
+  return contents
 }
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,7 @@
 
 const jwt = require('jsonwebtoken')
 
-import { verifyJwt } from '../src/index'
+import { verifyJwt, verifyAuthToken, verifyReauthToken } from '../src/index'
 
 describe('algorithm tests', () => {
 
@@ -46,4 +46,66 @@ describe('algorithm tests', () => {
     }).toThrow(/jwt signature is required/)
   })
 
+})
+
+describe('authtoken tests', () => {
+  const key = 'fB4KvCrgHH/OpLUQq+e2jWHVzEc'
+  const id = '51fcefc7-45cb-4ae5-9945-9b39817c6b24'
+  const payload = { id }
+
+  test('test return type', () => {
+    const token = jwt.sign(payload, key)
+    const verified = verifyAuthToken(token, key)
+    // this is more about verifying that the id field exists on the return
+    // type.
+    expect(verified.id).toBe(id)
+  })
+
+  test('test type guard', () => {
+    const badId = 'not a real id'
+    const token = jwt.sign({ id: badId }, key)
+
+    expect(() => {
+      verifyAuthToken(token, key)
+    }).toThrow(/malformed AuthToken/)
+  })
+})
+
+describe('reauthtoken tests', () => {
+  const key = 'fB4KvCrgHH/OpLUQq+e2jWHVzEc'
+  const operation = 'delete'
+  const id = 123
+  const userPayload = { operation, id }
+
+  test('test 1 methods required', () => {
+    const payload = {
+      userContents: JSON.stringify(userPayload),
+      reauthenticationMethods: ['password']
+    }
+    const token = jwt.sign(payload, key)
+    expect(() => {
+      verifyReauthToken(token, key, [])
+    }).toThrow(/Must supply at least one.*method/)
+  })
+
+  test('test methods present', () => {
+    const payload = {
+      userContents: JSON.stringify(userPayload),
+      reauthenticationMethods: ['password']
+    }
+    const token = jwt.sign(payload, key)
+    const verified = verifyReauthToken(token, key, ['password'])
+    expect(JSON.parse(verified.userContents)).toMatchObject(userPayload)
+  })
+
+  test('test methods missing', () => {
+    const payload = {
+      userContents: JSON.stringify(userPayload),
+      reauthenticationMethods: ['password']
+    }
+    const token = jwt.sign(payload, key)
+    expect(() => {
+      verifyReauthToken(token, key, ['mfa'])
+    }).toThrow(/The following required re-authentication methods were missing: mfa/)
+  })
 })
